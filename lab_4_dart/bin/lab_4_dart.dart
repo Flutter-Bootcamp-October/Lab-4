9b5e3230f48a0df373c11b61e7f96ab9834519af
Raffal Abu Eshey <86999942+Raff22@users.noreply.github.com>
2023-10-11 13:56:12 +0300

    added new code to dart

--- lab_4_dart/bin/lab_4_dart.dart
+++ lab_4_dart/bin/lab_4_dart.dart
@@ -1,5 +1,34 @@
+// import 'dart:html';
+
 import 'package:lab_4_dart/lab_4_dart.dart' as lab_4_dart;
 
 void main(List<String> arguments) {
-  print('Hello world: ${lab_4_dart.calculate()}!');
+  //task 1
+  List<String> names = ['Ahmad', 'Mohammed', 'Khalid'];
+  for (String name in names) {
+    print(name);
+  }
+  print('===================================');
+  print('');
+
+  //task2
+  List<String> days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
+  print(days.where((day) => day[0] == 'S').toList());
+  print('===================================');
+  print('');
+
+  //task3
+  Map<String, String> data = {'name': 'Ahmad', 'city': 'Riyadh', 'age': '24'};
+  data.removeWhere((key, value) => value.length < 4);
+  print(data);
+  print('===================================');
+  print('');
 }
